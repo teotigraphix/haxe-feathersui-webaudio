24ecc81b46e0f4ecbc8e6e48cd88b3c52445a14f
teotigraphix <mschmalle@teotigraphix.com>
2021-08-11 16:30:48 -0400

    Code cleanup.

--- audio-basics/src/Main.hx
+++ audio-basics/src/Main.hx
@@ -48,6 +48,11 @@ import openfl.events.Event;
 import openfl.events.MouseEvent;
 import openfl.net.URLRequest;
 
+// Editors Note:
+//   This is not how I would write an 'app' with these frameworks, purely demonstration.
+//   The ComponentFactrory serves as a Theme basically, and the constants are for ease
+//   of demonstration.
+
 /**
 	Translated with love;
 	@see https://mdn.github.io/webaudio-examples/audio-basics/
@@ -70,6 +75,7 @@ class Main extends Application {
 	private var panControl:HSlider;
 	private var powerControl:ToggleButton;
 
+	// Components
 	private var volumeLabel:Label;
 	private var panLabel:Label;
 
@@ -83,9 +89,9 @@ class Main extends Application {
 	private var componentsSection:LayoutGroup;
 	private var footerSection:LayoutGroup;
 
-	//
-	private var lastWidth:Float = 0;
-	private var lastHeight:Float = 0;
+	// Layout
+	private var lastWidth:Float = 0.0;
+	private var lastHeight:Float = 0.0;
 
 	private var isLandscape:Bool = false;
 	private var oldIslandscape:Bool = false;
@@ -104,8 +110,10 @@ class Main extends Application {
 
 		trace("Main.new()");
 
+		// In webaudio, the user needs to gesture first, before the AudioContext is created
 		stage.addEventListener(MouseEvent.MOUSE_DOWN, stage_mouseDownHandler, true);
 
+		// The SoundEngine decorates the audio node graph with API.
 		_soundEngine = new SoundEngine(_soundFile);
 	}
 
@@ -120,11 +128,13 @@ class Main extends Application {
 		hl.setPadding(ComponentFactory.LARGE_PADDING);
 		layout = hl;
 
+		// Creates the complete UI in sections
 		handleSection = createHandle();
 		controlsSection = createControlsSection();
 		componentsSection = createComponentsSection();
 		footerSection = createFooter();
 
+		// Add the sections in vertical order (VerticalLayout)
 		addChild(handleSection);
 		addChild(controlsSection);
 		addChild(componentsSection);
@@ -134,6 +144,7 @@ class Main extends Application {
 	override function update() {
 		super.update();
 
+		// If the dimensions of this component change, size will invalidate
 		var sizeInvalid = isInvalid(SIZE);
 		var sizeChanged:Bool = lastWidth != stage.stageWidth || lastHeight != stage.stageHeight;
 
@@ -151,7 +162,7 @@ class Main extends Application {
 
 	private function createHandle():LayoutGroup {
 		var group:LayoutGroup = new LayoutGroup();
-		group.layoutData = new VerticalLayoutData(100);
+		group.layoutData = new VerticalLayoutData(100.0);
 		group.height = ComponentFactory.CONTROLS_HEIGHT;
 		group.backgroundSkin = new HandleSkin();
 		return group;
@@ -165,26 +176,24 @@ class Main extends Application {
 
 		var section:LayoutGroup = new LayoutGroup();
 		section.height = ComponentFactory.COMPONENTS_HEIGHT;
-		section.layoutData = new VerticalLayoutData(100);
+		section.layoutData = new VerticalLayoutData(100.0);
 		section.layout = hl;
 		section.backgroundSkin = ComponentFactory.createRectangleSkin(8.0, ComponentFactory.blackColor, ComponentFactory.lightBlueColor, 16.0, 16.0);
 
-		////////////////
-
+		// Create Labels
 		volumeLabel = ComponentFactory.createLabel("VOLUME", "Helvetica", 20, ComponentFactory.labelColor, true);
 		panLabel = ComponentFactory.createLabel("PAN", "Helvetica", 20, ComponentFactory.labelColor, true);
 
-		//////////////////
 		// Create Controls
 		playControl = ComponentFactory.createPlayControl();
 		playControl.addEventListener(Event.CHANGE, playControl_changeHandler);
 
 		volumeControl = ComponentFactory.createVolumeSlider();
-		volumeControl.layoutData = new HorizontalLayoutData(100, 100);
+		volumeControl.layoutData = new HorizontalLayoutData(100.0, 100.0);
 		volumeControl.addEventListener(Event.CHANGE, volumeControl_changeHandler);
 
 		panControl = ComponentFactory.createVolumeSlider();
-		panControl.layoutData = new HorizontalLayoutData(100, 100);
+		panControl.layoutData = new HorizontalLayoutData(100.0, 100.0);
 		panControl.addEventListener(Event.CHANGE, panControl_changeHandler);
 
 		powerControl = ComponentFactory.createPowerControl();
@@ -198,9 +207,11 @@ class Main extends Application {
 		section.layoutData = new VerticalLayoutData(100, 100);
 		section.backgroundSkin = ComponentFactory.createRectangleSkin(8.0, ComponentFactory.blackColor, ComponentFactory.darkBlueColor, 16.0, 16.0);
 
+		// Create Components
 		speakerLeft = new SpeakerComponent();
 
 		eightTrack = new EightTrackComponent();
+		// Example of Composite Component
 		eightTrack.playControl = playControl;
 
 		speakerRight = new SpeakerComponent();
@@ -211,11 +222,11 @@ class Main extends Application {
 	private function createFooter():LayoutGroup {
 		var hl:HorizontalLayout = new HorizontalLayout();
 		hl.horizontalAlign = RIGHT;
-		hl.setPadding(16);
+		hl.setPadding(ComponentFactory.MEDIUM_PADDING);
 
 		var group:LayoutGroup = new LayoutGroup();
-		group.layoutData = new VerticalLayoutData(100);
-		group.height = 50;
+		group.layoutData = new VerticalLayoutData(100.0);
+		group.height = ComponentFactory.FOOTER_HEIGHT;
 		group.layout = hl;
 
 		var poweredBy = new PoweredByFeathersUI();
@@ -264,15 +275,15 @@ class Main extends Application {
 		hl.verticalAlign = MIDDLE;
 		controlsSection.layout = hl;
 
-		speakerLeft.layoutData = new HorizontalLayoutData(100, 100);
-		eightTrack.layoutData = new HorizontalLayoutData(100, 100);
-		speakerRight.layoutData = new HorizontalLayoutData(100, 100);
+		speakerLeft.layoutData = new HorizontalLayoutData(100.0, 100.0);
+		eightTrack.layoutData = new HorizontalLayoutData(100.0, 100.0);
+		speakerRight.layoutData = new HorizontalLayoutData(100.0, 100.0);
 
 		// Components
 		var hl:HorizontalLayout = new HorizontalLayout();
 		hl.verticalAlign = MIDDLE;
 
-		componentsSection.layoutData = new VerticalLayoutData(100, 100);
+		componentsSection.layoutData = new VerticalLayoutData(100.0, 100.0);
 		componentsSection.layout = hl;
 	}
 
@@ -293,11 +304,11 @@ class Main extends Application {
 		componentsSection.addChild(eightTrack);
 		componentsSection.addChild(speakerLeft);
 
-		speakerLeft.layoutData = new VerticalLayoutData(100, 100);
-		eightTrack.layoutData = new VerticalLayoutData(100, 100);
+		speakerLeft.layoutData = new VerticalLayoutData(100.0, 100.0);
+		eightTrack.layoutData = new VerticalLayoutData(100.0, 100.0);
 
 		var vl:VerticalLayout = new VerticalLayout();
-		componentsSection.layoutData = new VerticalLayoutData(100, 100);
+		componentsSection.layoutData = new VerticalLayoutData(100.0, 100.0);
 		componentsSection.layout = vl;
 	}
 
@@ -364,11 +375,12 @@ class Main extends Application {
 }
 
 class ComponentFactory {
-	public static var PADDING:Float = 8;
+	public static var PADDING:Float = 8.0;
 	public static var LARGE_PADDING:Float = PADDING * 4.0;
 	public static var MEDIUM_PADDING:Float = PADDING * 2.0;
 	public static var CONTROLS_HEIGHT:Float = 100.0;
 	public static var COMPONENTS_HEIGHT:Float = 200.0;
+	public static var FOOTER_HEIGHT:Float = 50.0;
 
 	public static var orangeColor:Int = 0xFF8800;
 	public static var greenColor:Int = 0x00FF00;
@@ -441,9 +453,9 @@ class ComponentFactory {
 		var skin = new RectangleSkin();
 		skin.border = SolidColor(strokeThickness, strokeColor);
 		skin.fill = SolidColor(fillColor);
-		if (width != 0)
+		if (width != 0.0)
 			skin.width = width;
-		if (height != 0)
+		if (height != 0.0)
 			skin.height = height;
 		return skin;
 	}
@@ -475,39 +487,39 @@ class SpeakerComponent extends FeathersControl {
 	private function drawBackground():Void {
 		graphics.clear();
 
-		var padding:Float = 42;
-		var padding2:Float = padding * 2;
+		var padding:Float = 42.0;
+		var padding2:Float = padding * 2.0;
 		var radius:Float = actualWidth > actualHeight ? actualHeight : actualWidth;
 		radius -= padding;
 
-		var centerX:Float = radius / 2;
-		var centerY:Float = radius / 2;
+		var centerX:Float = radius / 2.0;
+		var centerY:Float = radius / 2.0;
 
-		centerX = ((actualWidth) / 2);
-		centerY = ((actualHeight) / 2);
+		centerX = ((actualWidth) / 2.0);
+		centerY = ((actualHeight) / 2.0);
 
 		// Background
-		graphics.lineStyle(10, ComponentFactory.blackColor, 1);
+		graphics.lineStyle(10, ComponentFactory.blackColor, 1.0);
 		graphics.beginFill(ComponentFactory.darkBlueColor, 1.0);
 		graphics.drawRect(0, 0, actualWidth, actualHeight);
 		graphics.endFill();
 
 		// Circle 1
-		graphics.lineStyle(10, ComponentFactory.blackColor, 1);
+		graphics.lineStyle(10, ComponentFactory.blackColor, 1.0);
 		graphics.beginFill(ComponentFactory.lightBlueColor, 1.0);
-		graphics.drawCircle(centerX, centerY, (radius - 20) / 2);
+		graphics.drawCircle(centerX, centerY, (radius - 20.0) / 2.0);
 		graphics.endFill();
 
 		// Circle 2
-		graphics.lineStyle(4, ComponentFactory.blackColor, 1);
+		graphics.lineStyle(4, ComponentFactory.blackColor, 1.0);
 		graphics.beginFill(ComponentFactory.lightBlueColor, 1.0);
-		graphics.drawCircle(centerX, centerY, (radius - 100) / 2);
+		graphics.drawCircle(centerX, centerY, (radius - 100.0) / 2.0);
 		graphics.endFill();
 
 		// Circle 3
-		graphics.lineStyle(10, ComponentFactory.blackColor, 1);
+		graphics.lineStyle(10, ComponentFactory.blackColor, 1.0);
 		graphics.beginFill(ComponentFactory.darkBlueColor, 1.0);
-		graphics.drawCircle(centerX, centerY, 80 / 2);
+		graphics.drawCircle(centerX, centerY, 80.0 / 2.0);
 		graphics.endFill();
 	}
 }
@@ -558,7 +570,7 @@ class EightTrackComponent extends FeathersControl {
 
 			if (_playControl != null) {
 				_playControl.validateNow();
-				_playControl.move((actualWidth - _playControl.width) / 2, actualHeight - _playControl.height - 20);
+				_playControl.move((actualWidth - _playControl.width) / 2, actualHeight - _playControl.height - 20.0);
 			}
 		}
 	}
@@ -573,8 +585,8 @@ class EightTrackComponent extends FeathersControl {
 		graphics.endFill();
 
 		var padding:Float = 40;
-		var rectWidth:Float = actualWidth - (padding * 2);
-		var rectHeight:Float = actualHeight / 3;
+		var rectWidth:Float = actualWidth - (padding * 2.0);
+		var rectHeight:Float = actualHeight / 3.0;
 
 		// 8Track Player
 		var radius = rectHeight;
@@ -583,17 +595,17 @@ class EightTrackComponent extends FeathersControl {
 		graphics.drawRoundRect(padding, rectHeight, rectWidth, rectHeight, radius);
 		graphics.endFill();
 
-		var radius = rectHeight / 4;
+		var radius = rectHeight / 4.0;
 		// Circle Left
 		graphics.lineStyle(8, ComponentFactory.blackColor);
 		graphics.beginFill(ComponentFactory.darkBlueColor, 1.0);
-		graphics.drawCircle(padding + padding + radius, rectHeight + (rectHeight / 2), radius);
+		graphics.drawCircle(padding + padding + radius, rectHeight + (rectHeight / 2.0), radius);
 		graphics.endFill();
 
 		// Circle Right
 		graphics.lineStyle(8, ComponentFactory.blackColor);
 		graphics.beginFill(ComponentFactory.darkBlueColor, 1.0);
-		graphics.drawCircle(actualWidth - (padding + padding + radius), rectHeight + (rectHeight / 2), radius);
+		graphics.drawCircle(actualWidth - (padding + padding + radius), rectHeight + (rectHeight / 2.0), radius);
 		graphics.endFill();
 	}
 }
@@ -611,12 +623,12 @@ class PowerButtonSkin extends ProgrammaticSkin {
 
 		graphics.clear();
 
-		var centerX = actualWidth / 2;
-		var centerY = actualHeight / 2;
-		var radius = actualHeight / 2;
+		var centerX = actualWidth / 2.0;
+		var centerY = actualHeight / 2.0;
+		var radius = actualHeight / 2.0;
 
 		// Outer Circle
-		graphics.lineStyle(8, ComponentFactory.blackColor, 1);
+		graphics.lineStyle(8, ComponentFactory.blackColor, 1.0);
 		graphics.beginFill(ComponentFactory.orangeColor, 1.0);
 		graphics.drawCircle(centerX, centerY, radius);
 		graphics.endFill();
@@ -630,12 +642,12 @@ class PowerButtonSkin extends ProgrammaticSkin {
 		// Vertical back ink
 		graphics.lineStyle(12, ComponentFactory.orangeColor, 1, false, LineScaleMode.NORMAL, ROUND);
 		graphics.moveTo(centerX, centerY);
-		graphics.lineTo(centerX, 16);
+		graphics.lineTo(centerX, 16.0);
 
 		// Vertical ink
 		graphics.lineStyle(8, ComponentFactory.blackColor, 1, false, LineScaleMode.NORMAL, ROUND);
 		graphics.moveTo(centerX, centerY);
-		graphics.lineTo(centerX, 16);
+		graphics.lineTo(centerX, 16.0);
 	}
 }
 
@@ -650,7 +662,7 @@ class PlayButtonSkin extends BaseGraphicsPathSkin {
 	override function drawPath() {
 		// Triangle
 		graphics.moveTo(0, 0);
-		graphics.lineTo(actualWidth, actualHeight / 2);
+		graphics.lineTo(actualWidth, actualHeight / 2.0);
 		graphics.lineTo(0, actualHeight);
 	}
 }
@@ -663,19 +675,19 @@ class HandleSkin extends ProgrammaticSkin {
 	override function update() {
 		graphics.clear();
 
-		var padding:Float = 50;
+		var padding:Float = 50.0;
 		var halfHeight:Float = actualHeight / 2;
-		var contentWidth:Float = (actualWidth - (padding * 2));
+		var contentWidth:Float = (actualWidth - (padding * 2.0));
 
 		// Handle
-		graphics.lineStyle(10, 1);
-		graphics.beginFill(ComponentFactory.orangeColor, 1);
+		graphics.lineStyle(10.0, 1.0);
+		graphics.beginFill(ComponentFactory.orangeColor, 1.0);
 		graphics.drawRect(padding, 0, contentWidth, halfHeight);
 		graphics.endFill();
 
 		// Hole
-		graphics.lineStyle(10, 1);
-		graphics.beginFill(ComponentFactory.backgroundColor, 1);
+		graphics.lineStyle(10.0, 1.0);
+		graphics.beginFill(ComponentFactory.backgroundColor, 1.0);
 		graphics.drawRect(padding, halfHeight, contentWidth, halfHeight);
 		graphics.endFill();
 	}
@@ -801,7 +813,7 @@ class SoundEngine {
 
 		var audioSource:MediaElementAudioSourceNode = _audioContext.createMediaElementSource(cast(_audioElement, MediaElement));
 
-		var pannerOptions:StereoPannerOptions = {pan: 0};
+		var pannerOptions:StereoPannerOptions = {pan: 0.0};
 
 		_gainNode = _audioContext.createGain();
 		_panNode = new StereoPannerNode(_audioContext, pannerOptions);
